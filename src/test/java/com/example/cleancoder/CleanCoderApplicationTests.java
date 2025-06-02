1276955bf8dcdff8fc2ba434a035ff37f682803a
leejinwoo <leejinwoo1126@gmail.com>
2025-06-02 13:43:06 +0900

    git init

---
+++ src/test/java/com/example/cleancoder/CleanCoderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.cleancoder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CleanCoderApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
