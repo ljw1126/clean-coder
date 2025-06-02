120c0bcfbc849cbc0170925d62e24ffdf649fe97
leejinwoo <leejinwoo1126@gmail.com>
2025-06-02 15:00:39 +0900

    refactor: ArrayList 상속 제거 및 리스트 필드 추가 [#OOP2]

--- src/main/java/com/example/cleancoder/oop2/inheritance/LuggageCompartment.java
+++ src/main/java/com/example/cleancoder/oop2/inheritance/LuggageCompartment.java
@@ -1,27 +1,27 @@
 package com.example.cleancoder.oop2.inheritance;
 
 import java.util.ArrayList;
+import java.util.List;
 
-public class LuggageCompartment extends ArrayList<Luggage> {
+public class LuggageCompartment {
+    private List<Luggage> luggages = new ArrayList<>();
     private int restSpace;
 
     public LuggageCompartment(int restSpace) {
         this.restSpace = restSpace;
     }
 
-    @Override
     public boolean add(Luggage luggage) {
         this.restSpace -= luggage.size();
-        return super.add(luggage);
+        return luggages.add(luggage);
     }
 
     public boolean canContain(Luggage luggage) {
         return this.restSpace > luggage.size();
     }
 
-    // super.remove()가 public이라서 노출됨 (상속 자체를 잘못 사용)(
     public void extract(Luggage luggage) {
         this.restSpace += luggage.size();
-        super.remove(luggage);
+        luggages.remove(luggage);
     }
 }
--- src/test/java/com/example/cleancoder/oop2/inheritance/LuggageCompartmentTest.java
+++ src/test/java/com/example/cleancoder/oop2/inheritance/LuggageCompartmentTest.java
@@ -24,7 +24,7 @@ class LuggageCompartmentTest {
             assertThat(lc.canContain(luggage)).isFalse();
         }
 
-        lc.remove(luggage); // restSpace 공간이 추가되야 하는데 상속 남용으로 잘못 사용해 비즈니스 로직을 거치지 못함
+        lc.extract(luggage);
         assertThat(lc.canContain(luggage)).isTrue();
     }
 }
