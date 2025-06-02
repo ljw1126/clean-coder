65a670dc807d8123424338f9f39420509dea4365
이진우 <leejinwoo1126@gmail.com>
2025-06-02 20:13:36 +0900

    feat: PrintPrimes 예제 추가

---
+++ src/test/java/com/example/cleancoder/function/FileContentComparator.java
@@ -0,0 +1,22 @@
+package com.example.cleancoder.function;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+
+import static org.assertj.core.api.Assertions.*;
+
+public class FileContentComparator {
+    public void compare() {
+        try {
+            BufferedReader lead = new BufferedReader(new FileReader("lead"));
+            BufferedReader gold = new BufferedReader(new FileReader("src/test/java/function/gold"));
+            String line;
+            while ((line = gold.readLine()) != null)
+                assertThat(line).isEqualTo(lead.readLine());
+
+            assertThat(lead.readLine()).isNull();
+        } catch (Exception e) {
+            e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
+        }
+    }
+}
---
+++ src/test/java/com/example/cleancoder/function/PrintPrimesTest.java
@@ -0,0 +1,11 @@
+package com.example.cleancoder.function;
+
+import org.junit.jupiter.api.Test;
+
+public class PrintPrimesTest {
+
+    @Test
+    void makeSureMatchesGold() {
+        PrintPrimes.main(new String[0]);
+    }
+}
