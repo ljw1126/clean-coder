e3a2bd0dba88362066671d78873d1015dd94dc07
이진우 <leejinwoo1126@gmail.com>
2025-06-03 20:03:10 +0900

    refactor: Movie switch문에 다형성 적용, 서브 클래스 생성

---
+++ src/main/java/com/example/cleancoder/functionstructure/NewReleaseMovie.java
@@ -0,0 +1,17 @@
+package com.example.cleancoder.functionstructure;
+
+public class NewReleaseMovie extends Movie {
+  public NewReleaseMovie(String title) {
+    super(title, Movie.NEW_RELEASE);
+  }
+
+  @Override
+  double determineAmount(int daysRented) {
+    return daysRented * (double)3;
+  }
+
+  @Override
+  int determineFrequentRentalPoint(int daysRented) {
+    return daysRented > 1 ? 2 : 1;
+  }
+}
---
+++ src/main/java/com/example/cleancoder/functionstructure/RegularMovie.java
@@ -0,0 +1,22 @@
+package com.example.cleancoder.functionstructure;
+
+public class RegularMovie extends Movie {
+  public RegularMovie(String title) {
+    super(title, Movie.REGULAR);
+  }
+
+  @Override
+  double determineAmount(int daysRented) {
+    double rentalAmount = 2;
+    if(daysRented > 2) {
+      rentalAmount += (daysRented - 2) * 1.5;
+    }
+
+    return rentalAmount;
+  }
+
+  @Override
+  int determineFrequentRentalPoint(int daysRented) {
+    return 1;
+  }
+}
