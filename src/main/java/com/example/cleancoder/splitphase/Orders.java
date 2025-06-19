ca972facbbba6ec16748ef421c61f1e1fafa07f1
leejinwoo <leejinwoo1126@gmail.com>
2025-06-19 22:27:05 +0900

    refactor: Orders Split Phase

--- src/main/java/com/example/cleancoder/splitphase/Orders.java
+++ src/main/java/com/example/cleancoder/splitphase/Orders.java
@@ -4,15 +4,26 @@ record Product(double basePrice, double discountThreshold, double discountRate) 
 
 record ShippingMethod(double discountThreshold, double discountedFee, Double feePerCase) {}
 
+record PricingData(double basePrice, double discount, int quantity) {}
+
 public class Orders {
     double priceOrder(Product product, int quantity, ShippingMethod shippingMethod) {
+        PricingData pricingData = calculatePricingData(product, quantity);
+        return applyShippingCost(shippingMethod, pricingData);
+    }
+
+    private static PricingData calculatePricingData(Product product, int quantity) {
         double basePrice = product.basePrice() * quantity;
         double discount = Math.max(quantity - product.discountThreshold(), 0)
                 * product.basePrice() * product.discountRate();
-        double shippingPerCase = (basePrice > shippingMethod.discountThreshold())
+
+        return new PricingData(basePrice, discount, quantity);
+    }
+
+    private static double applyShippingCost(ShippingMethod shippingMethod, PricingData pricingData) {
+        double shippingPerCase = (pricingData.basePrice() > shippingMethod.discountThreshold())
                 ? shippingMethod.discountedFee() : shippingMethod.feePerCase();
-        double shippingCost = quantity * shippingPerCase;
-        double price = basePrice - discount * shippingCost;
-        return price;
+        double shippingCost = pricingData.quantity() * shippingPerCase;
+        return pricingData.basePrice() - pricingData.discount() * shippingCost;
     }
 }
